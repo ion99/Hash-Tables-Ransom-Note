15bb31b481ad89e00830c79d3b864011d4f928e1
Ion Gorincioi <gorincioii@yahoo.com>
2016-10-02 15:43:12 -0700

    Complete the solution for the challenge in Javascript

---
+++ ransomNote.js
@@ -0,0 +1,15 @@
+function ransomNote(strMagazine, strRansom) {
+  let magazine = strMagazine.split(" ");
+  let ransom = strRansom.split(" ");
+    for (let i = 0; i < magazine.length; i++) {
+      if(ransom.includes(magazine[i])){
+        ransom.splice(ransom.indexOf(magazine[i]), 1);
+      }
+      if (!ransom.length){
+        return "Yes";
+      }
+    }
+    return "No";
+}
+
+//ransomNote("you and me have only one reason to remember this day", "you have only one day");
